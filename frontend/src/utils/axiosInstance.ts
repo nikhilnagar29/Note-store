d8bfe72b266cbf175ac22a61bec88c5bf7621972
Nikhil nagar <nsquarepart1@gmail.com>
2025-08-30 22:35:48 +0530

    Fix deploy problem

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -5,6 +5,8 @@ const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000', // backend API base URL
 });
 
+// console.log("API instance created with base URL:", import.meta.env.VITE_BACKEND_URL);
+
 // Add Authorization header automatically
 api.interceptors.request.use((config: any) => {
   const token = localStorage.getItem('token');
